7fd7fc4c3453dbe4105f8c394c6e164f90abc51a
ShivamSV1 <131441312+ShivamSV1@users.noreply.github.com>
2025-03-01 14:46:49 +0530

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -6,25 +6,29 @@ class MyDocument extends Document {
     return (
       <Html lang="en" className="theme-compiled">
         <Head>
-
-<!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-ZRJM59G927"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-ZRJM59G927');
-</script>
-
+          {/* Google tag (gtag.js) */}
+          <script
+            async
+            src="https://www.googletagmanager.com/gtag/js?id=G-ZRJM59G927"
+          ></script>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', 'G-ZRJM59G927');
+              `,
+            }}
+          />
         </Head>
         <body
-          className={`antialiased text-lg bg-white dark:bg-gray-900 dark:text-white leading-base`}
+          className="antialiased text-lg bg-white dark:bg-gray-900 dark:text-white leading-base"
         >
           {/* Google Tag Manager (noscript) */}
           <noscript
             dangerouslySetInnerHTML={{
-              __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=G-ZRJM59G92"
+              __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=G-ZRJM59G927"
               height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
             }}
           />
@@ -37,4 +41,3 @@ class MyDocument extends Document {
 }
 
 export default MyDocument;
-
